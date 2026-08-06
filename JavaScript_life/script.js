53d2c57e1eda8f40044fe94ea387e7f6dd79641a
Pavithra2807 <pavithragowda2807@gmail.com>
2026-08-06 16:54:25 +0530

    variable_declaration

---
+++ JavaScript_life/script.js
@@ -0,0 +1,8 @@
+// var (variable Declaration) is used to declare a variable in JavaScript.
+var name; //decl in same line
+name="Pavithra"; //initialize in same line
+console.log(name); 
+
+var name; //re-declaration is allowed 
+name="pavi" //re-initialization is allowed
+console.log(name);
