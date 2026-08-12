5ab3d14dccd595ea5a1ae3340c0ff7fbd103a6dd
Pavithra2807 <pavithragowda2807@gmail.com>
2026-08-12 16:59:27 +0530

    Callback ,Hof - function

--- JavaScript_life/Operators/AssignmentOperators.js
+++
@@ -1,8 +0,0 @@
-// 2. Assignment Operators
-let x=10;
-let y=5;
-console.log(x+=y);
-console.log(x-=y);
-console.log(x*=y);
-console.log(x/=y);
-console.log(x%=y);
---
+++ JavaScript_life/Truthy_Falsy_values/Truthy_Falsy.js
@@ -0,0 +1,10 @@
+// Truthy & Falsy Values
+
+// Falsy Values ---> false,0,-0, 0n, "", undefined, null, NaN.
+
+if(""){
+    console.log("This is correct✅")
+}
+else{
+    console.log("This is Wrong❌")
+}
